f70bebad681643a90886a66d6e748b8a46287ce4
anjana502 <anjana.nallanagula@gmail.com>
2026-02-23 17:59:41 +0000

    Code challenge submission for question: 122 Best Time to Buy and Sell Stock II

---
+++ 122 Best Time to Buy and Sell Stock II/122best-time-to-buy-and-sell-stock-ii.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        sum1 = 0
+
+        for i in range(len(prices) - 1):
+            if (prices[i] < prices[i + 1]):
+                sum1 += (prices[i + 1] - prices[i])
+        
+        return sum1
