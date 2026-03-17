e4de6290f612c49d52fc69eef7f73e05c14596ff
anjana502 <anjana.nallanagula@gmail.com>
2026-03-17 12:57:37 +0000

    Code challenge submission for question: 20 Valid Parentheses

---
+++ 20 Valid Parentheses/20valid-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = []
+
+        for i in s:
+            if (i in "([{"):
+                stack.append(i)
+            else:
+                if ((len(stack) == 0) or (i == ")" and stack[-1] != "(") or (i == "]" and stack[-1] != "[") or (i == "}" and stack[-1] != "{")):
+                    return False
+                
+                stack.pop()
+        
+        if (len(stack) != 0):
+            return False
+        return True
