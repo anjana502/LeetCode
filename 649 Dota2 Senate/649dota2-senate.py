b25257ea33f4ddfea29d072c4fdc49fbad37b307
anjana502 <anjana.nallanagula@gmail.com>
2026-03-31 17:20:02 +0000

    Code challenge submission for question: 649 Dota2 Senate

---
+++ 649 Dota2 Senate/649dota2-senate.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        r_q = deque()
+        d_q = deque()
+        n = len(senate)
+
+        for i in range(n):
+            if (senate[i] == "R"):
+                r_q.append(i)
+            else:
+                d_q.append(i)
+        
+        while (len(r_q) != 0 and len(d_q) != 0):
+            if (r_q[0] < d_q[0]):
+                d_q.popleft()
+                index = r_q.popleft()
+                r_q.append(index + n)
+            else:
+                r_q.popleft()
+                index = d_q.popleft()
+                d_q.append(index + n)
+        
+        if (len(r_q) != 0):
+            return "Radiant"
+        return "Dire"
